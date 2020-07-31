440bb729791cf9ad4245b4fd230eeefdd5fb371a
George Talnagi <gtalnagi@yahoo.com>
2020-07-31 23:16:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 
 
 def long_planeteer_calls(short_words)
-  short_words.any? { |i.length > 4| }
+  short_words.any? { |short_words.length > 4| }
 end
 
 def find_the_cheese# code an argument here
