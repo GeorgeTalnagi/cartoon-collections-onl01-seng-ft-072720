d1f577ae208f80ece159c16adab0ae6750d91d19
George Talnagi <gtalnagi@yahoo.com>
2020-07-31 23:37:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,8 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = planeteer_calls.split
   planeteer_calls.collect do |elements|
-  planeteer_calls << "#{!}"
   end
-  planeteer_calls.join(" ")
+  planeteer_calls.join("!")
 end
 
 
